7199a73e36f5c8c19e86d0e18a6b14ef6b1e2c63
Fred Morrison <frederick.morrison@outlook.com>
2025-06-05 10:26:38 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+//
+import {About} from './about/about';
+import {Contact} from './contact/contact';
+import {Home} from './home/home';
+import {PageNotFound} from './page-not-found/page-not-found';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'home', component: Home, title: 'Home'},
+    {path: 'about', component: About, title: 'About'},
+    {path: 'contact', component: Contact, title: 'Contact'},
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: '**', component: PageNotFound}
+];
---
+++ src/app/navigation-bar/navigation-bar.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+//
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+//
+import {CustomLink} from './custom-link';
+
+@Component({
+    selector: 'app-navigation-bar',
+    imports: [MatGridListModule, MatToolbarModule, RouterLink, RouterLinkActive],
+    templateUrl: './navigation-bar.html',
+    styleUrl: './navigation-bar.scss'
+})
+export class NavigationBar implements OnInit {
+    links: CustomLink[] = [];
+
+    constructor(private router: Router) {
+    }
+
+    ngOnInit(): void {
+        // build the navigation links using the Angular route config that was defined in app.routes.ts
+        // and dependency-injected into this component, which effectively shares that route information without
+        // having to define a separate Angular service component.
+        for (const route of this.router.config) {
+            // we only care about the ones that have a title attribute,
+            // since the others are not going to be rendered in our navigation bar
+            if (route.title) {
+                //WARNING: must use string interpolation to convert title from Resolve<string> to just string
+                const link: CustomLink = {
+                    path: `/${route.path}`,
+                    label: `${route.title}`
+                };
+                this.links.push(link);
+            }
+        }
+        console.log(JSON.stringify(this.links));
+    }
+
+}
